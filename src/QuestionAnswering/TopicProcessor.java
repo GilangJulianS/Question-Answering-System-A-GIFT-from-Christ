747ca3c5e1bcc2aee83b47e05078377f9fe2a0e4
felicia-christie <waxrain1217@gmail.com>
2015-11-05 23:27:37 +0700

    driver TopicProcessor (run TopicProcessor-nya)

--- src/QuestionAnswering/TopicProcessor.java
+++ src/QuestionAnswering/TopicProcessor.java
@@ -3,13 +3,31 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package QuestionAnswering;
+
+import IndonesianNLP.IndonesianPOSTagger;
+import java.util.List;
+import model.Entity;
+import tubes.TestPostTag;
 
 /**
  *
  * @author SPM
  */
 public class TopicProcessor {
+
+    public static void processQuery(String query) {
+	  query = query.replaceAll("#[\\w]*|[\\S]+\\.[\\S]+/[\\S]+", " "); // remove hashtag + url
+	  query = query.replaceAll("[!\"$'()*+/:;,<=>?@\\^_`{|}]+", " "); //remove punctiation
+	  
+	  List<String[]> sentence = IndonesianPOSTagger.doPOSTag(query);
+	  for(int i = 0; i < sentence.size(); i++){
+		System.out.println(sentence.get(i)[0] + " " + sentence.get(i)[1]);
+	  }
+	  
+    }
     
+    public static void main(String[] args){
+	  processQuery("Berapa diskon pizza tanggal 21-11-2012?");
+    }
 }
